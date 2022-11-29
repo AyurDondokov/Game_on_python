a8bf7a7fc5049e8378e85baf20e3797e125b9889
anrew1002 <91276494+anrew1002@users.noreply.github.com>
2022-11-29 19:20:49 +0800

    Создал main.py и properties.py
    мэйн содержит основной цикл игры в классе Game.
    properties - переменные настроек

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from properties import *
+
+"""Основной скелет игры, класс Game"""
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while not (self.game_over):
+            for event in pygame.event.get():  # Блок с отловом событий в дальнейшем перекачует в отдельный метод класса Game
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            self.screen.fill('black')
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run
