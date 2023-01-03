947aab095e9a555bbc72931a70d7ba5d3ab3fa6f
Zueva Natalia <natta_liya@vk.com>
2023-01-03 15:55:53 +0800

    изменения

--- replicas_data.py
+++ replicas_data.py
@@ -1,19 +1,17 @@
 from reader_dialog import *
-import logging as log
 
-# список с персонажами
-r_c = {
-    "Ar": "Аюр:",
-    "H": "Ты:"
-}
+
 check = ReadingLocations('dialog/proba.txt')
-test_npc=check.dia_loc('caravan_lider__1')
+test_npc = check.dia_loc('caravan_lider__1')
+test_npc2 = check.dia_loc('caravan_lider__2')
+npc = check.dict_npc_loc
+print(npc)
 
-# test_npc = check.conversation
-print(test_npc)
-test_npc2=check.dia_loc('caravan_lider__2')
-# test_npc2 = check.conversation
-
+# # список с персонажами
+# r_c = {
+#     "Ar": "Аюр:",
+#     "H": "Ты:"
+# }
 # test_npc = [f"{r_c['Ar']}Hello",
 #             f"{r_c['Ar']}My name is Ayur",
 #             f"{r_c['Ar']}Its first dialog in game",
