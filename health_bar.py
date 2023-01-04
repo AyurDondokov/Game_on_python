4d1e7ab841def04739faf8b4a305193281312f5c
Юрий Вербитский 24 <000333555666@mail.ru>
2023-01-04 16:07:20 +0800

    Health_bar обновление

--- health_bar.py
+++ health_bar.py
@@ -11,17 +11,35 @@ class HealthBar(pygame.sprite.Sprite):
         # В LEVELS_PROPERTIES 1 словарь, где здоровье = 100
         # После прокачки level_health будет увеличен на единицу
         self.level_health = 1
-        # Уровни здоровья, брони и урона
+        # НЕизменяемая шкала здоровья
         self.health = LEVELS_PROPERTIES[self.level_health]['max_health']
+        # Изменяемая шкала здоровья
+        self.health_bar = LEVELS_PROPERTIES[self.level_health]['max_health']
+        # Броня
         self.armor = LEVELS_PROPERTIES[self.level_health]['armor']
+        # Урон
         self.damage = LEVELS_PROPERTIES[self.level_health]['max_damage']
+        # Лечение
+        self.heal = 25
 
+        # Отрисовка шкалы здоровья (Уровень самого здоровья)
+        # pygame.draw.rect(window, (60, 255, 15), (15, 30, health_bar, 25))
+        # Отрисовка шкалы здоровья (Заполненная шкала)
+        # pygame.draw.rect(window, (255, 255, 255), (15, 30, self.health, 25))
+
+    # Функция в случае если игрок проиграл
+    def game_over(self):
+        pass
+
+    # Нанесение дамага
     def takeDamage(self):
-        self.health -= (self.damage - self.armor)
-        if self.health < 0:
-            self.health = 0
+        self.health_bar -= (self.damage - self.armor)
+        if self.health_bar < 0:
+            self.health_bar = 0
+            self.game_over()
 
-    def Heal(self, heal):
-        self.health += heal
-        if self.health > 5:
-            self.health = 5
+    # Лечение
+    def Heal(self):
+        self.health_bar += self.heal
+        if self.health_bar >= self.health_bar:
+            self.health_bar = self.health_bar
