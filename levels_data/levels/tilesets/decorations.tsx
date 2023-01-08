8dbf5f0e74d3ba69473347050a687497461e771b
p1petto <108504552+p1petto@users.noreply.github.com>
2023-01-09 03:12:58 +0800

    decorations desert

---
+++ levels_data/levels/tilesets/decorations.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="decorations" tilewidth="192" tileheight="192" tilecount="22" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="60" height="64" source="../../graphics/desert_tileset/bone_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="65" height="64" source="../../graphics/desert_tileset/bone_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../graphics/desert_tileset/bone_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="33" source="../../graphics/desert_tileset/stone_3.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../../graphics/desert_tileset/cactus_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../../graphics/desert_tileset/cactus_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../../graphics/desert_tileset/grass_1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="30" height="32" source="../../graphics/desert_tileset/grass_2.png"/>
+ </tile>
+ <tile id="8">
+  <image width="26" height="32" source="../../graphics/desert_tileset/grass_3.png"/>
+ </tile>
+ <tile id="9">
+  <image width="138" height="160" source="../../graphics/desert_tileset/palm.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../graphics/desert_tileset/ruin_1.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="192" source="../../graphics/desert_tileset/ruin_2.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../../graphics/desert_tileset/ruin_3.png"/>
+ </tile>
+ <tile id="13">
+  <image width="192" height="64" source="../../graphics/desert_tileset/ruin_4.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../graphics/desert_tileset/shrub.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../graphics/desert_tileset/stone_1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../../graphics/desert_tileset/stone_2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="../../graphics/desert_tileset/tumbleweed.png"/>
+ </tile>
+ <tile id="18">
+  <image width="135" height="126" source="../../graphics/desert_tileset/camel_ass.png"/>
+ </tile>
+ <tile id="19">
+  <image width="135" height="126" source="../../graphics/desert_tileset/camel_left1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="135" height="99" source="../../graphics/desert_tileset/camel_lying.png"/>
+ </tile>
+ <tile id="21">
+  <image width="135" height="126" source="../../graphics/desert_tileset/wagon.png"/>
+ </tile>
+</tileset>
